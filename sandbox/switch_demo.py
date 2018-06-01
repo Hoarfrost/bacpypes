c0e76c7ae5bfcccf3094c197324ce7053347e787
Joel Bender <joel@carrickbender.com>
2018-05-31 23:42:04 -0400

    reference terminals by name, add activative/deactivate function lookup, add/del terminals

--- sandbox/switch_demo.py
+++ sandbox/switch_demo.py
@@ -19,11 +19,34 @@ this_console = None
 this_switch = None
 
 #
+#   DebugTerm
+#
+
+class DebugTerm(Debug):
+
+    """
+    A simple wrapper around the Debug class that prints out when it has been
+    activated and deactivated by the switch.
+    """
+
+    def activate(self):
+        print(self.label + " activated")
+
+    def deactivate(self):
+        print(self.label + " deactivated")
+
+#
 #    Switch
 #
 
 @bacpypes_debugging
 class Switch(Client, Server):
+
+    """
+    A Switch is a client and server that wraps around clients and/or servers
+    and provides a way to switch between them without unbinding and rebinding
+    the stack.
+    """
 
     class TerminalWrapper(Client, Server):
 
@@ -31,7 +54,10 @@ class Switch(Client, Server):
             self.switch = switch
             self.terminal = terminal
 
-            bind(self, terminal, self)
+            if isinstance(terminal, Server):
+                bind(self, terminal)
+            if isinstance(terminal, Client):
+                bind(terminal, self)
 
         def indication(self, *args, **kwargs):
             self.switch.request(*args, **kwargs)
@@ -39,29 +65,78 @@ class Switch(Client, Server):
         def confirmation(self, *args, **kwargs):
             self.switch.response(*args, **kwargs)
 
-    def __init__(self, *terminals):
+    def __init__(self, **terminals):
         if _debug: Switch._debug("__init__ %r", terminals)
 
         Client.__init__(self)
         Server.__init__(self)
 
         # wrap the terminals
-        self.terminals = [Switch.TerminalWrapper(self, terminal) for terminal in terminals]
-        self.current_terminal = 0
+        self.terminals = {k:Switch.TerminalWrapper(self, v) for k, v in terminals.items()}
+        self.current_terminal = None
 
-    def switch_terminal(self, indx):
-        if (indx < 0) or (indx >= len(self.terminals)):
-            raise IndexError("terminal index out of range")
+    def __getitem__(self, key):
+        if key not in self.terminals:
+            raise KeyError("%r not a terminal" % (key,))
 
-        self.current_terminal = indx
+        # return the terminal, not the wrapper
+        return self.terminals[key].terminal
+
+    def __setitem__(self, key, term):
+        if key in self.terminals:
+            raise KeyError("%r already a terminal" % (key,))
+
+        # build a wrapper and map it
+        self.terminals[key] = Switch.TerminalWrapper(self, term)
+
+    def __delitem__(self, key):
+        if key not in self.terminals:
+            raise KeyError("%r not a terminal" % (key,))
+
+        # if deleting current terminal, deactivate it
+        term = self.terminals[key].terminal
+        if term is self.current_terminal:
+            terminal_deactivate = getattr(self.current_terminal, 'deactivate', None)
+            if terminal_deactivate:
+                terminal_deactivate()
+            self.current_terminal = None
+
+        del self.terminals[key]
+
+    def switch_terminal(self, key):
+        if key not in self.terminals:
+            raise KeyError("%r not a terminal" % (key,))
+
+        if self.current_terminal:
+            terminal_deactivate = getattr(self.current_terminal, 'deactivate', None)
+            if terminal_deactivate:
+                terminal_deactivate()
+
+        if key is None:
+            self.current_terminal = None
+        else:
+            self.current_terminal = self.terminals[key].terminal
+            terminal_activate = getattr(self.current_terminal, 'activate', None)
+            if terminal_activate:
+                terminal_activate()
 
     def indication(self, *args, **kwargs):
         """Downstream packet, send to current terminal."""
-        self.terminals[self.current_terminal].terminal.indication(*args, **kwargs)
+        if not self.current_terminal:
+            raise RuntimeError("no active terminal")
+        if not isinstance(self.current_terminal, Server):
+            raise RuntimeError("current terminal not a server")
+
+        self.current_terminal.indication(*args, **kwargs)
 
     def confirmation(self, *args, **kwargs):
         """Upstream packet, send to current terminal."""
-        self.terminals[self.current_terminal].terminal.confirmation(*args, **kwargs)
+        if not self.current_terminal:
+            raise RuntimeError("no active terminal")
+        if not isinstance(self.current_terminal, Client):
+            raise RuntimeError("current terminal not a client")
+
+        self.current_terminal.confirmation(*args, **kwargs)
 
 #
 #   TestConsoleCmd
@@ -97,8 +172,24 @@ class TestConsoleCmd(Client, Server, ConsoleCmd):
         if _debug: TestConsoleCmd._debug("do_switch %r", args)
         global this_switch
 
-        this_switch.switch_terminal(int(args[0]))
+        this_switch.switch_terminal(args[0])
         print("switched")
+
+    def do_add(self, args):
+        args = args.split()
+        if _debug: TestConsoleCmd._debug("do_add %r", args)
+        global this_switch
+
+        # make a new terminal
+        this_switch[args[0]] = DebugTerm(args[0])
+
+    def do_del(self, args):
+        args = args.split()
+        if _debug: TestConsoleCmd._debug("do_del %r", args)
+        global this_switch
+
+        # delete the terminal
+        del this_switch[args[0]]
 
     def indication(self, arg):
         """Got a request, echo it back up the stack."""
@@ -121,11 +212,11 @@ def main():
     if _debug: _log.debug("    - args: %r", args)
 
     # make some debugging terminals
-    debug1 = Debug("1")
-    debug2 = Debug("2")
+    debug1 = DebugTerm("a")
+    debug2 = DebugTerm("b")
 
     # make a switch with them
-    this_switch = Switch(debug1, debug2)
+    this_switch = Switch(a=debug1, b=debug2)
     if _debug: _log.debug("    this_switch: %r", this_switch)
 
     # make a test console
